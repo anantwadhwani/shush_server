28c5d1bdbe6a69ab9d549f22d7048f2d17b3a4a2
Anant Wadhwani <anantwadhwani2017@gmail.com>
2025-02-15 14:12:48 +0530

    localhostCORS

--- index.mjs
+++ index.mjs
@@ -8,19 +8,27 @@ monogoConnect();
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 const corsOptions = {
-    origin: "https://shush-w3d5.onrender.com", // frontend URI (ReactJS)
-}
+    origin: ["https://shush-w3d5.onrender.com", "http://localhost:3000"], // frontend URI (ReactJS)
+};
 app.use(cors(corsOptions));
 
 // Available Routes
 app.use('/auth', auth);
 app.use('/shush', shush);
 
+// app.get("/*", function (req, res) {
+//     res.sendFile("C:/Users/91878/Dropbox/PC/Desktop/React/shush/public/index.html", function (err) {
+//         if (err) {
+//             res.status(500).send(err);
+//         }
+//     });
+// });
+
 const port = 4000;
 app.listen(port, () => {
-    console.log(`Shush Backend server running at ${port}`)
-})
+    console.log(`Shush Backend server running at ${port}`);
+});
